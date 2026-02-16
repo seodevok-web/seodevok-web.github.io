b454bd96ae7d4c470578bf4be7451833ebe72431
Edu Nezzan <seodevok@gmail.com>
2026-02-16 19:42:51 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Contoh untuk fungsi tombol (cta-button) jika diperlukan
+document.querySelector('.cta-button').addEventListener('click', function() {
+    alert('You clicked the Get Started button!');
+});
